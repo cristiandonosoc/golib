6e703d741d8bb79fedfeb8b1abc4dd92f8c6b2e7
Cristián Donoso <cristiandonosoc@gmail.com>
2024-02-08 23:14:53 -0500

    [files] Add File Cache and permit overwriting of keys

--- pkg/files/file_cache.go
+++ pkg/files/file_cache.go
@@ -1,72 +1,69 @@
 package files
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
 )
 
+// Main API ----------------------------------------------------------------------------------------
+
+// LoadFileFromPath attempts to load a file from a path and will store it in the global cache.
+func LoadFileFromPath(path string) (*LoadedFile, error) {
+	key, err := filepath.Abs(path)
+	if err != nil {
+		return nil, fmt.Errorf("abs %q: %w", path, err)
+	}
+	return GlobalFileCache().LoadFromPath(key, path, false)
+}
+
+// LoadFileFromPathWithKey is a more advanced way of loading files that permit to insert it in an
+// specific key, rather than using the abs path, as it is normally done.
+// |overwrite| refers to whether we allow people to overwrite keys or not.
+func LoadFileFromPathWithKey(key, path string, overwrite bool) (*LoadedFile, error) {
+	return GlobalFileCache().LoadFromPath(key, path, overwrite)
+}
+
+// QueryKey checks to see if the key is in the global cache, and if so, returns the loaded file.
+// For files loaded as path, the key would the absolute path as returned by |filepath.Abs|.
+func QueryKey(key string) (bool, *LoadedFile) {
+	return GlobalFileCache().QueryKey(key)
+}
+
+// NewFromData creates a in-memory loaded file from the given data and stores it in the cache with
+// the given key. From that moment on, it works similarly to a loaded file read from a file.
+// |overwrite| refers to whether we allow people to overwrite keys or not.
+func NewFromData(key string, data []byte, overwrite bool) (*LoadedFile, error) {
+	return GlobalFileCache().NewFromData(key, data, overwrite)
+}
+
+// Cache Implementation ----------------------------------------------------------------------------
+
 var once sync.Once
 var gFileCache *fileCache
 
 // FileCache represents a view to files loaded in memory.
 type fileCache struct {
-	files map[string]*loadedFile
+	files map[string]*LoadedFile
+	// DisallowKeyCollision determines whether the cache will look for key collisions.
+	// Normally we disallow this for tests.
+	DisallowKeyCollision bool
 }
 
 func GlobalFileCache() *fileCache {
 	once.Do(func() {
-		gFileCache = &fileCache{}
+		gFileCache = &fileCache{
+			files:                map[string]*LoadedFile{},
+			DisallowKeyCollision: false,
+		}
 	})
 
 	return gFileCache
 }
 
-type loadedFile struct {
-	Key   string
-	Data  []byte
-	lines []string
-
-	FromFile bool
-}
-
-// Path returns the Key as a path if the file was loaded from file rather than a buffer.
-// Returns empty otherwise.
-func (lf *loadedFile) Path() string {
-	if lf.FromFile {
-		return lf.Key
-	}
-
-	return ""
-}
-
-// Lines lazily parses the content of the file into lines.
-func (lf *loadedFile) Lines() ([]string, error) {
-	// Check if the lines have already been loaded.
-	if lf.lines != nil {
-		return lf.lines, nil
-	}
-
-	scanner := bufio.NewScanner(bytes.NewReader(lf.Data))
-
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("scanning file line by line: %w", err)
-	}
-
-	lf.lines = lines
-	return lf.lines, nil
-}
-
 // QueryKey checks the cache to see if that key has already been loaded.
-func (fc *fileCache) QueryKey(key string) (bool, *loadedFile) {
+func (fc *fileCache) QueryKey(key string) (bool, *LoadedFile) {
 	if file, ok := fc.files[key]; ok {
 		return true, file
 	}
@@ -76,35 +73,32 @@ func (fc *fileCache) QueryKey(key string) (bool, *loadedFile) {
 
 // LoadFromPath creates a new loaded file from a path.
 // The key of the file will be the absolute path of the file.
-func (fc *fileCache) LoadFromPath(path string) (*loadedFile, error) {
-	abs, err := filepath.Abs(path)
-	if err != nil {
-		return nil, fmt.Errorf("abs %q: %w", path, err)
-	}
-
+func (fc *fileCache) LoadFromPath(key, path string, overwrite bool) (*LoadedFile, error) {
 	// Check if the file is already read.
-	if file, ok := fc.files[abs]; ok {
+	if file, ok := fc.files[key]; ok {
 		return file, nil
 	}
 
-	data, err := os.ReadFile(abs)
+	data, err := os.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("reading %q: %w", abs, err)
+		return nil, fmt.Errorf("reading %q: %w", path, err)
 	}
 
-	return fc.NewFromData(abs, data)
+	return fc.NewFromData(key, data, overwrite)
 }
 
 // NewFromData creates a new loadedFile with the provided key and content.
 // The key must not be in use already.
 // This is normally used for in-memory files, usually for testing purposes.
-func (fc *fileCache) NewFromData(key string, data []byte) (*loadedFile, error) {
+func (fc *fileCache) NewFromData(key string, data []byte, overwrite bool) (*LoadedFile, error) {
 	// We should not have the key already.
-	if _, ok := fc.files[key]; ok {
+	if !fc.DisallowKeyCollision && !overwrite {
+		if _, ok := fc.files[key]; ok {
+		}
 		return nil, fmt.Errorf("key %q is already in use", key)
 	}
 
-	file := &loadedFile{
+	file := &LoadedFile{
 		Key:  key,
 		Data: data,
 	}
---
+++ pkg/files/loaded_file.go
@@ -0,0 +1,62 @@
+package files
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+)
+
+type LoadedFile struct {
+	Key   string
+	Data  []byte
+	lines []string
+
+	FromFile bool
+}
+
+// LoadedFilePosition represents a single position (character) within a loaded file.
+type LoadedFilePosition struct {
+	File *LoadedFile
+	Line int
+	Char int
+}
+
+func NewLoadedFilePosition(lf *LoadedFile, line, char int) *LoadedFilePosition {
+	return &LoadedFilePosition{
+		File: lf,
+		Line: line,
+		Char: char,
+	}
+}
+
+// Path returns the Key as a path if the file was loaded from file rather than a buffer.
+// Returns empty otherwise.
+func (lf *LoadedFile) Path() string {
+	if lf.FromFile {
+		return lf.Key
+	}
+
+	return ""
+}
+
+// Lines lazily parses the content of the file into lines.
+func (lf *LoadedFile) Lines() ([]string, error) {
+	// Check if the lines have already been loaded.
+	if lf.lines != nil {
+		return lf.lines, nil
+	}
+
+	scanner := bufio.NewScanner(bytes.NewReader(lf.Data))
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanning file line by line: %w", err)
+	}
+
+	lf.lines = lines
+	return lf.lines, nil
+}
