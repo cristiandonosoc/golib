e804c8220979431253d13177bce6209bda18a483
Cristián Donoso <cristiandonosoc@gmail.com>
2024-02-18 23:15:09 -0500

    [files] Add copy directory command
    Also update Stat and other minor functions

--- pkg/files/files.go
+++ pkg/files/files.go
@@ -3,9 +3,18 @@ package files
 
 import (
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 )
+
+// ToUnixPath standardizes the path to be Unix-like. This is useful for making paths work
+// uniformly between Windows and Linux.
+func ToUnixPath(path string) string {
+	return strings.ReplaceAll(path, "\\", "/")
+}
 
 // DirExists check whether the directory exists and is a directory (not another type of file).
 func DirExists(path string) (bool, error) {
@@ -23,15 +32,106 @@ func DirExists(path string) (bool, error) {
 
 // StatFile returns the file info of the file if it can be done.
 // If the file does not exists, the returned file info will be nil.
-func StatFile(path string) (fs.FileInfo, error) {
+func StatFile(path string) (fs.FileInfo, bool, error) {
 	stat, err := os.Stat(path)
 	if err != nil {
 		if os.IsNotExist(err) {
-			stat = nil
+			return nil, false, nil
 		} else {
-			return nil, err
+			return nil, false, err
 		}
 	}
 
-	return stat, nil
+	return stat, true, nil
+}
+
+// CopyFile
+func CopyFile(src, dst string) error {
+	return CopyFileAdvanced(src, dst, CopyFileAdvancedOptions{
+		Sync: false,
+	})
+}
+
+type CopyFileAdvancedOptions struct {
+	// DstCreateDir determines whether to try to create the owning directory of the dst file.
+	DstCreateDir bool
+	// Sync ensures any buffered data is sent immediatelly.
+	Sync bool
+}
+
+func CopyFileAdvanced(src, dst string, options CopyFileAdvancedOptions) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("opening %q: %w", src, err)
+	}
+	defer srcFile.Close()
+
+	if options.DstCreateDir {
+		dir := filepath.Dir(dst)
+		if err := os.MkdirAll(dir, 0644); err != nil {
+			return fmt.Errorf("mkdirall %q: %w", dir, err)
+		}
+	}
+
+	// Create (or truncate) the destination file.
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("opening %q: %w", dst, err)
+	}
+	defer dstFile.Close()
+
+	if _, err := io.Copy(dstFile, srcFile); err != nil {
+		return fmt.Errorf("copying data from %q to %q: %w", src, dst, err)
+	}
+
+	if options.Sync {
+		if err := dstFile.Sync(); err != nil {
+			return fmt.Errorf("calling sync on %q: %w", dst, err)
+		}
+	}
+
+	return nil
+}
+
+// CopyDirRecursive copies all the content of a directory into another path.
+func CopyDirRecursive(from, to string) error {
+	// TODO(cdc): Make this use errgroup.
+	from = filepath.Clean(from)
+
+	var files []string
+	err := filepath.WalkDir(from, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		// Remove the prefix from the path.
+		path = ToUnixPath(strings.TrimPrefix(path, from))
+		path = strings.TrimPrefix(path, "/")
+		files = append(files, path)
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("walking %q: %w", from, err)
+	}
+
+	to = filepath.Clean(to)
+
+	for _, file := range files {
+		src := filepath.Join(from, file)
+		dst := filepath.Join(to, file)
+
+		options := CopyFileAdvancedOptions{
+			DstCreateDir: true,
+		}
+		if err := CopyFileAdvanced(src, dst, options); err != nil {
+			return fmt.Errorf("copying %q -> %q: %w", src, dst, err)
+		}
+	}
+
+	return nil
 }
