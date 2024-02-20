72300a4bd1219bce1dacf75c242976f8cb06d243
Cristián Donoso <cristiandonosoc@gmail.com>
2024-02-19 22:41:29 -0500

    [test_support] Add test support initial functions
    These permit to acquire the runfiles from either go or bazel.

---
+++ pkg/test_support/test_support.go
@@ -0,0 +1,140 @@
+// Package test_support holds common testing utilities.
+package test_support
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel"
+	"github.com/cristiandonosoc/golib/pkg/files"
+)
+
+var bazelCheckOnce sync.Once
+var gRunningAsBazelTest bool
+
+func runningAsBazelTest() bool {
+	bazelCheckOnce.Do(func() {
+		gRunningAsBazelTest = os.Getenv("BAZEL_TEST") == "1"
+	})
+
+	return gRunningAsBazelTest
+}
+
+// TestTmpDir returns a valid base string to be fed to os.MkdirTemp.
+func TestTmpBase() string {
+	if runningAsBazelTest() {
+		return os.Getenv("TEST_TMPDIR")
+	}
+
+	// Fallback to the system default.
+	return ""
+}
+
+// Runfiles returns a list of all the runfiles associated with this test that contains |dir|.
+// Typical use is Runfiles("testdata")
+func Runfiles(dir string) ([]string, error) {
+	var candidates []string
+	if runningAsBazelTest() {
+		bazelCandidates, err := bazelCandidatesRunfiles(dir)
+		if err != nil {
+			return nil, fmt.Errorf("reading bazel runfiles: %w", err)
+		}
+		candidates = bazelCandidates
+	} else {
+		// Otherwise we open the dir and list it.
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			return nil, fmt.Errorf("opening dir %q: %w", dir, err)
+		}
+
+		for _, entry := range entries {
+			candidates = append(candidates, filepath.Join(dir, entry.Name()))
+		}
+	}
+
+	// For now we just query single level. This could be extended for recursive files.
+	result := make([]string, 0, len(candidates))
+	for _, candidate := range candidates {
+		stat, err := os.Stat(candidate)
+		if err != nil {
+			return nil, fmt.Errorf("os stat %q: %w", candidate, err)
+		}
+
+		if stat.IsDir() {
+			continue
+
+		}
+
+		result = append(result, candidate)
+	}
+
+	return result, nil
+}
+
+func bazelCandidatesRunfiles(dir string) ([]string, error) {
+	// We attempt to query Bazel to see if it can find runfiles.
+	runfiles, err := bazel.ListRunfiles()
+	if err != nil {
+		return nil, fmt.Errorf("listing runfiles: %w", err)
+	}
+
+	var candidates []string
+	if len(runfiles) > 0 {
+		for _, rf := range runfiles {
+			if !strings.Contains(files.ToUnixPath(rf.ShortPath), dir) {
+				continue
+			}
+
+			candidates = append(candidates, rf.Path)
+		}
+	}
+
+	return candidates, nil
+}
+
+// RunfilePath tries to find a testdata file in a build system agnostic way, working for both Bazel
+// environments and default Go ones.
+func RunfilePath(path string) (string, error) {
+	if runningAsBazelTest() {
+		// We attempt to query Bazel to see if it can find runfiles.
+		runfiles, err := bazel.ListRunfiles()
+		if err != nil {
+			return "", fmt.Errorf("listing runfiles: %w", err)
+		}
+
+		// If we get runfiles, then we are running in a Bazel mode, so we return the files from there.
+		if len(runfiles) > 0 {
+			// Bazel returns the runfiles as relative from the workspace root, so for now we do a simple
+			// suffix match. This might need some more thought in the future.
+			path = files.ToUnixPath(path)
+			for _, rf := range runfiles {
+				if strings.HasSuffix(files.ToUnixPath(rf.Path), path) {
+					return rf.Path, nil
+				}
+			}
+
+		}
+
+		return "", fmt.Errorf("cannot find %q", path)
+	}
+
+	// If not, we attempt to find the file normally, as that would work for normal Go invocations.
+	if _, found, err := files.StatFile(path); err != nil || !found {
+		return "", fmt.Errorf("stat %q: %w (found: %t)", path, err, found)
+	}
+
+	return path, nil
+}
+
+// LoadRunfile tries to read a file using the loading rules of |RunfilePath|.
+func LoadRunfile(path string) (*files.LoadedFile, error) {
+	rp, err := RunfilePath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return files.LoadFileFromPath(rp)
+}
